dce4f6b3178418d1c4dac399e05bdf5e87c4c996
onemore-creator <alextiulip@gmail.com>
2025-09-02 15:26:35 +0200

    Initial Commit

---
+++ NeonLightsTestApp/NeonLightsTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeonLightsTestAppApp.swift
+//  NeonLightsTestApp
+//
+//  Created by Aleksandr Tiulpanov on 02/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct NeonLightsTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NeonLightsTestAppTests/NeonLightsTestAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  NeonLightsTestAppTests.swift
+//  NeonLightsTestAppTests
+//
+//  Created by Aleksandr Tiulpanov on 02/09/2025.
+//
+
+import Testing
+@testable import NeonLightsTestApp
+
+struct NeonLightsTestAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
